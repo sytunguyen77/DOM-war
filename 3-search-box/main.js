0e6f0ea261a31495e458a48fa324237826cb82d3
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2024-12-14 22:22:30 -0600

    add comment

--- 3-search-box/main.js
+++ 3-search-box/main.js
@@ -5,7 +5,7 @@
 
 const searchButton = document.querySelector(".search-btn");
 searchButton.addEventListener("click", () => {
-  const parent = searchButton.parentElement;
-  parent.classList.toggle("open");
-  parent.querySelector("input").focus();
+  const parent = searchButton.parentElement; // searchButton.parentElement: Lấy phần tử cha trực tiếp của searchButton. Mục đích: Truy cập vào phần tử cha (container của nút và input).
+  parent.classList.toggle("open"); // Thêm class nếu nó chưa tồn tại hoặc xóa class nếu nó đã tồn tại
+  parent.querySelector("input").focus(); // Đặt con trỏ vào input để người dùng có thể nhập ngay lập tức
 });
